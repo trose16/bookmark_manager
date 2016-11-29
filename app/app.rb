c05b38e26d5f9e9e67b86d71c65fc0a5b6ee3620
Agata Sumowska <agata.sumowska@gmail.com>
2016-11-29 16:52:50 +0000

    Adds a makers link and displays it on the site

--- app/app.rb
+++ app/app.rb
@@ -1,10 +1,15 @@
 require 'sinatra/base'
+require './app/models/link'
 
 class Bookmark_manager < Sinatra::Base
   get '/' do
     erb(:index)
   end
 
+  get '/links' do
+    @links = Link.all
+    erb(:'links/index')
+  end
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
