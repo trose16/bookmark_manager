d472aa6db6ac85aacaac1b62820ea6255c07d531
Teako Warfield-Graham <teakowg@gmail.com>
2016-12-01 12:20:31 +0000

    Cleaning up git

--- app/models/link.rb
+++ app/models/link.rb
@@ -6,10 +6,10 @@ class Link
 
   include DataMapper::Resource
 
-  has n, :tags, through: Resource
-
   property :id, Serial
   property :name, String
   property :url, String
+
+  has n, :tags, through: Resource
 
 end
